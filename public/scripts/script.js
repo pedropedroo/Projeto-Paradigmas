3ac36915e977d329fb4f6ad870948eda95eaa250
pedropedroo <pedrootaviop2001@gmail.com>
2024-04-04 22:58:30 -0300

    Adicionada aba de leitura

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -30,11 +30,50 @@ botao.addEventListener('click', () => {
         if (!isNaN(desconto)) {
             valorFinal -= (desconto / 100) * valorFinal;
         }
-          
+
         document.querySelector('#valor_final').value = valorFinal.toFixed(2);
     }else{
         alert("Insira as informações corretamente");
     }
-
-    
 });
+
+let botao2 = document.querySelector('#botao_vender').addEventListener('click', () => {
+    const campos = document.querySelectorAll('#nome_cliente, #telefone, #produto, #lente,#desconto, #valor_final');
+    let preenchido = true;
+
+    campos.forEach(campo => {
+        if (campo.value === "") {
+            preenchido = false;
+            return;
+        }
+    });
+
+    if (preenchido) {
+        const nome = document.querySelector('#nome_cliente').value;
+        const telefone = document.querySelector('#telefone').value;
+        const produto = document.querySelector('#produto').value;
+        const lente = document.querySelector('#lente').value;
+        const desconto = document.querySelector('#desconto').value;
+        let valorFinal = parseFloat(document.querySelector('#valor_final').value);
+        
+        const novaLinha = document.createElement('tr');
+        novaLinha.innerHTML = `
+            <td>${nome}</td>
+            <td>${telefone}</td>
+            <td>${produto}</td>
+            <td>${lente}</td>
+            <td>${desconto + "%"}</td>
+            <td>${valorFinal.toFixed(2)}</td>
+        `;
+    
+        document.querySelector('#tabela_vendas tbody').appendChild(novaLinha);
+
+        campos.forEach(campo => {
+            campo.value = "";
+        });
+    } else {
+        alert("Preencha todas as informações");
+    }
+});
+
+
